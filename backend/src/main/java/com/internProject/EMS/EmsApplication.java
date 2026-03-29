7ec07a0a1654ec077243497bb574ea8d3a153aa8
rahul kumar <rkprasad1928@gmail.com>
2026-03-30 01:11:26 +0530

    db connected succesfully

---
+++ backend/src/main/java/com/internProject/EMS/EmsApplication.java
@@ -0,0 +1,14 @@
+package com.internProject.EMS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmsApplication.class, args);
+		System.out.println("Employee Management System Project is Runinng");
+	}
+
+}
